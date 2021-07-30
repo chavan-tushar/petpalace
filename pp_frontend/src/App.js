d17480c8a97a5fbc682cbc1c6001cf27dceb021c
chavan-tushar <tushu01@gmail.com>
2021-07-30 23:44:14 +0100

    updation

--- pp_frontend/src/App.js
+++ pp_frontend/src/App.js
@@ -4,10 +4,11 @@ import Header from './pp_components/Header'
 import Footer from './pp_components/Footer'
 import HomeScreen from './pp_screens/HomeScreen'
 import ProductScreen from './pp_screens/ProductScreen'
-
 import CartScreen from './pp_screens/CartScreen'
-
-
+import LoginScreen from './pp_screens/LoginScreen'
+import RegisterScreen from './pp_screens/RegisterScreen'
+import ProfileScreen from './pp_screens/ProfileScreen'
+import ShippingScreen from './pp_screens/ShippingScreen'
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
         </Container>
