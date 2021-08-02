f9b5e3b9e799557873622875228b3aab6b2738a2
chavan-tushar <tushu01@gmail.com>
2021-08-02 19:14:09 +0100

    added /static

--- pp_frontend/src/pp_components/Product.js
+++ pp_frontend/src/pp_components/Product.js
@@ -7,7 +7,7 @@ function Product({ product }) {
     return (
         <Card className="my-3 p-3 rounded" >
             <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} />
+                <Card.Img src={`/static${product.image}`} />
             </Link>
 
             <Card.Body>
