299ebbb7d8587d12b479273b7a87e30899c42a57
chavan-tushar <64707012+chavan-tushar@users.noreply.github.com>
2021-08-02 17:08:28 +0100

    Update ProductScreen.js

--- pp_frontend/src/pp_screens/ProductScreen.js
+++ pp_frontend/src/pp_screens/ProductScreen.js
@@ -36,7 +36,7 @@ function ProductScreen({ match, history }) {
                     : (
                         <Row>
                             <Col md={6}>
-                                <Image src={product.image} alt={product.name} fluid/>
+                                <Image src={`/static/${product.image}`} alt={product.name} fluid/>
                             </Col>
                             <Col md={3}>
                                 <ListGroup variant="flush">
